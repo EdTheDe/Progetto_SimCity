7c30b7b1a48d8f0bc7071cdd9946cad664bed8b9
Niko141 <nico.ambrosi4@gmail.com>
2026-05-17 17:37:42 +0200

    Aggiunta Classe base per entità urbane (UrbanEntity base class)

---
+++ Codice/UrbanEntity.java
@@ -0,0 +1,29 @@
+// Nicolò
+package citylogic.domain.entities;
+
+/**
+ * Classe base astratta per ogni entità posizionabile sulla griglia (UrbanGrid).
+ * Segue il Domain Design Model.
+ */
+public abstract class UrbanEntity {
+    
+    private final double placementCost;
+    private int developmentLevel;
+
+    public UrbanEntity(double placementCost) {
+        this.placementCost = placementCost;
+        this.developmentLevel = 1; // Default starting level (KAN-21)
+    }
+
+    public double getPlacementCost() {
+        return placementCost;
+    }
+
+    public int getDevelopmentLevel() {
+        return developmentLevel;
+    }
+
+    public void upgradeLevel() {
+        this.developmentLevel++;
+    }
+}
