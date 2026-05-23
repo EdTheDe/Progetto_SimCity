d9982b3a98babd59c9b8c40522e289ea818c93e0
Niko141 <nico.ambrosi4@gmail.com>
2026-05-23 12:03:18 +0200

    UrbanGrid - lista edifici - Metodo per piazzare edifici

--- src/main/java/citylogic/domain/map/UrbanGrid.java
+++ src/main/java/citylogic/domain/map/UrbanGrid.java
@@ -1,6 +1,10 @@
 // Nicolò
 package citylogic.domain.map;
 
+import java.util.List;
+import java.util.ArrayList;
+import citylogic.domain.entities.UrbanEntity;
+
 /**
  * Mappa della simulazione (logica).
  * Gestisce array bidimensionali di oggetti Cell.
@@ -10,6 +14,7 @@ public class UrbanGrid {
     private final int width;
     private final int height;
     private final Cell[][] grid;
+    private List<UrbanEntity> activeEntities;   // Nuova lista per tenere traccia di tutto ciò che è costruito
 
     /**
      * Costruttore di default. 
@@ -20,6 +25,7 @@ public class UrbanGrid {
         this.height = 20;
         this.grid = new Cell[width][height];
         initializeGrid();
+        this.activeEntities = new ArrayList<>(); // lista aggiunta alla griglia
     }
 
     private void initializeGrid() {
@@ -53,5 +59,31 @@ public class UrbanGrid {
             throw new IllegalArgumentException("Coordinates out of bounds: (" + x + ", " + y + ")");
         }
         return grid[x][y];
+    }
+
+    /**
+     * Per ciclare la lista degli edifici.
+     * @return
+     */
+    public List<UrbanEntity> getActiveEntities() {
+        return activeEntities;
+    }
+
+    /**
+     * Piazza un'entità sulla griglia se la cella è valida e libera.
+     * @param entity L'entità da piazzare (es. Residential, Commercial)
+     * @param x Coordinata X
+     * @param y Coordinata Y
+     */
+    public void placeEntity(UrbanEntity entity, int x, int y) {
+        // 1. Recupera la cella in sicurezza (il controllo Out-of-Bounds è già in getCell!)
+        Cell targetCell = getCell(x, y);
+
+        // 2. Assegna l'entità.
+        // Se la cella è occupata, setEntity() in Cell.java lancerà in automatico la IllegalStateException
+        targetCell.setEntity(entity);
+
+        // 3. Se la riga sopra va a buon fine (nessuna eccezione), registriamo l'edificio
+        activeEntities.add(entity);
     }
 }
