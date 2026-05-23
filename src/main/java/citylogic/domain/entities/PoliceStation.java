4b353eefe0bbfdab669eedefc123f677d802d275
Niko141 <nico.ambrosi4@gmail.com>
2026-05-23 13:33:53 +0200

    Aggiunta edifici statali

---
+++ src/main/java/citylogic/domain/entities/PoliceStation.java
@@ -0,0 +1,8 @@
+//N
+package citylogic.domain.entities;
+
+public class PoliceStation extends StateBuilding {
+    public PoliceStation(double placementCost, double baseMaintenanceCost, int baseSecurityCapacity) {
+        super(placementCost, baseMaintenanceCost, baseSecurityCapacity);
+    }
+}
